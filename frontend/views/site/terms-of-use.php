25f9cb626a223e616ac122cec4811dd281403edb
strahinja <strahinjamomirov@gmail.com>
2018-07-23 20:24:55 +0200

    refactor(TermsOfUse)

--- frontend/views/site/terms-of-use.php
+++ frontend/views/site/terms-of-use.php
@@ -6,11 +6,84 @@ use yii\helpers\Html;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+Yii::$app->params['bodyClass'] = 'confess-page';
+
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div id="info-content">
+    <h2 style="color=#eee">Terms and Conditions</h2>
+    <div class="post-wrap">
+        <br>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+        <h3>Terms and Conditions Last updated: May 16, 2018</h3>
+        <p>Please read these Terms and Conditions carefully before using the confessr.com website.
+            Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms.
+            These Terms apply to all visitors, users and others who access or use the Service.
+            By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the
+            terms then you may not access the Service.
+        </p>
 
-    <code><?= __FILE__ ?></code>
+        <h3>Privacy</h3>
+        <p>Your privacy is safe with us!
+            Confessr.com is committed to protecting the privacy of its users and providing them with the most powerful
+            and safe online experience.
+            Confessr.com does not collect your personal data like your address, phone number. The only data that is
+            collected includes only information about your internet provider that you use to access the internet,
+            including your IP address and your browser type. This information is used to improve the site and adapt it
+            to the needs of the users.
+            Confessr.com does not use such information to identify its visitors, never will use it in such way and will
+            not disclose such information.
+            The Service obliges to guard the privacy of its users, except in case of serious breaching of the law or
+            illegal activity of its users.
+        </p>
+        <h3>Materials on confessr.com</h3>
+        <p>Confessr.com does not claim ownership of the materials you provide to the Service (including feedback and
+            suggestions) or post, upload, input or submit to confessr.com. However, by posting, uploading, inputting,
+            providing or submitting your Submission you are granting confessr.com permission to use your Submission in
+            connection with the operation of their Internet businesses including, without limitation, the rights to:
+            copy, distribute, transmit, publicly display, publicly perform, reproduce, edit, translate and reformat your
+            Submission.
+            Confessr.com has no obligation to monitor the communication services on its site. However, confessr.com
+            reserves the right to review materials posted and to remove any materials in its sole discretion.
+            Confessr.com reserves the right to terminate your access to any or all of the communication services at any
+            time without notice.</p>
+
+        <h3>Links To Other Web Sites</h3>
+        <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by
+            Confessr.com.
+            Confessr.com has no control over, and assumes no responsibility for, the content, privacy policies, or
+            practices of any third party web sites or services. You further acknowledge and agree that Confessr.com
+            shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be
+            caused by or in connection with use of or reliance on any such content, goods or services available on or
+            through any such web sites or services.
+            We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or
+            services that you visit.</p>
+
+        <h3>Termination</h3>
+        <p>We may terminate or suspend access to our Service immediately, without prior notice or liability, for any
+            reason whatsoever, including without limitation if you breach the Terms.</p>
+
+        <h3>Changes</h3>
+        <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time.
+            By continuing to access or use our Service after those revisions become effective, you agree to be bound by
+            the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+
+        <h3>Contact Us</h3>
+        <p>If you have any questions about these Terms, please contact us.</p>
+
+    </div>
 </div>
+<style>
+    #info-content {
+        width: 700px;
+        margin: 0 auto;
+        padding: 30px 25px 80px 25px;
+        color: white;
+        background: none;
+    }
+
+    .post-wrap {
+        padding: 20px;
+        margin: 20px;
+        background: rgba(50, 50, 50, 0.8);
+    }
+</style>
